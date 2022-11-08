3572c3fc469d598e2b44d4d908c0dc31272ba2fe
BasantaBabu <basantab.ghimire@gmail.com>
2022-11-08 12:39:00 -0500

    commit image

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-//import { Router } from '@angular/router';
+
 import { UserService } from 'src/app/services/user.service';
-//import { UserService } from 'src/app/services/user.service'; // user authentication
+
 
 @Component({
   selector: 'app-signup',
@@ -11,7 +11,7 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class SignupComponent implements OnInit {
 
-  imageUrl ='https://scontent.fyto1-1.fna.fbcdn.net/v/t1.6435-9/62463661_10157386106568331_1829929532958703616_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=enEg0K3pUNAAX_dNjCR&_nc_ht=scontent.fyto1-1.fna&oh=00_AT8NLnlDIHOcf8SneRQtXlDYtZE0-gV3PkUrKd4fa7BuvQ&oe=6351BBB8';
+  imageUrl ='../../../assets/phot01.png';
  //email Validation
  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   
